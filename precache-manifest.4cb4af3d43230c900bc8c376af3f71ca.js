ba07f21924d06a827c72808a4fce3ef51b005d32
maqsoodlivepin <maqsood@livepin.tv>
2020-02-07 02:48:49 +0500

    Updates

---
+++ precache-manifest.4cb4af3d43230c900bc8c376af3f71ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e75a136eae5bdfbf83ce72b164f7e7",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7ec6e5013f3ebc511df0",
+    "url": "/my-portfolio/static/css/2.3b0ca333.chunk.css"
+  },
+  {
+    "revision": "64bf85f3b521f8c34c82",
+    "url": "/my-portfolio/static/css/main.088c5b8f.chunk.css"
+  },
+  {
+    "revision": "7ec6e5013f3ebc511df0",
+    "url": "/my-portfolio/static/js/2.b08bfdb2.chunk.js"
+  },
+  {
+    "revision": "64bf85f3b521f8c34c82",
+    "url": "/my-portfolio/static/js/main.3600a616.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "a2ebac2db9efbcb8925201595540ca31",
+    "url": "/my-portfolio/static/media/LINE_Chat_test_Assignment.a2ebac2d.png"
+  },
+  {
+    "revision": "e34683425f1f25abe924513107d8535c",
+    "url": "/my-portfolio/static/media/LivePinApp.e3468342.png"
+  },
+  {
+    "revision": "cdaa1f7e3ecbfc82c261d73b25196d20",
+    "url": "/my-portfolio/static/media/LivePinSite.cdaa1f7e.png"
+  },
+  {
+    "revision": "f68c326577b987c37927547ef9f7aeab",
+    "url": "/my-portfolio/static/media/LivePinSite0.f68c3265.png"
+  },
+  {
+    "revision": "eb6927150adc791a03076aa18a6a4b11",
+    "url": "/my-portfolio/static/media/maqsood.eb692715.JPG"
+  },
+  {
+    "revision": "9958701cd6bb15d1dff5d2efa0477aa2",
+    "url": "/my-portfolio/static/media/whatsapp.9958701c.png"
+  }
+]);
